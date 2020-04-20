e894f1f94232ed7d62aa2e68c2605224bf1b9e0a
hobroker <igor.leahu24@gmail.com>
2020-04-20 17:29:56 +0300

    Add default theme

---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final defaultTheme = ThemeData(
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.black,
+  brightness: Brightness.dark,
+  backgroundColor: Colors.black54,
+  accentColor: Colors.white,
+  accentIconTheme: IconThemeData(color: Colors.black),
+  dividerColor: Colors.black12,
+);
