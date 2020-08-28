9c820e8ca8e63fe32ce44de639262e3569c86a8b
hobroker <igor.leahu24@gmail.com>
2020-08-28 11:37:43 +0300

    Add AssetPath helper

---
+++ lib/modules/asset_path.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+@immutable
+class AssetPath {
+  static final i18nDir = 'assets/i18n';
+}
--- lib/modules/i18n_delegate.dart
+++ lib/modules/i18n_delegate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_i18n/loaders/decoders/json_decode_strategy.dart';
+import 'package:thynui/modules/asset_path.dart';
 import 'package:thynui/util/debug.dart';
 
 void _missingTranslationHandler(key, locale) {
@@ -11,7 +12,7 @@ void _missingTranslationHandler(key, locale) {
 var i18nDelegate = FlutterI18nDelegate(
   translationLoader: FileTranslationLoader(
     fallbackFile: 'en',
-    basePath: 'assets/i18n',
+    basePath: AssetPath.i18nDir,
     decodeStrategies: [JsonDecodeStrategy()],
   ),
   missingTranslationHandler: _missingTranslationHandler,
