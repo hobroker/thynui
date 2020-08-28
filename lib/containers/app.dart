a4a30fb51837f22ff54b4ca0da157bde7ade17f2
hobroker <igor.leahu24@gmail.com>
2020-08-28 11:55:00 +0300

    Add redux store to App
    dispatch AppStartedAction when App loads

--- lib/containers/app.dart
+++ lib/containers/app.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:thynui/actions/app_actions.dart';
+import 'package:thynui/models/state/app_state.dart';
 import 'package:thynui/modules/i18n_delegate.dart';
 import 'package:thynui/modules/theme.dart';
 import 'package:thynui/screens/login/login_screen.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  final Store<AppState> store;
+
+  const App({Key key, this.store}) : super(key: key);
+
+  void onInit() {
+    store.dispatch(AppStartedAction());
+  }
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    widget.onInit();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Thyn',
-      theme: defaultTheme,
-      home: LoginScreen(),
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: [i18nDelegate],
-      supportedLocales: [Locale('en', '')],
-      builder: FlutterI18n.rootAppBuilder(),
+    return StoreProvider(
+      store: widget.store,
+      child: MaterialApp(
+        title: 'Thyn',
+        theme: defaultTheme,
+        home: LoginScreen(),
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: [i18nDelegate],
+        supportedLocales: [Locale('en', '')],
+        builder: FlutterI18n.rootAppBuilder(),
+      ),
     );
   }
 }
