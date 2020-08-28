098ae374f62f80dd9c7b1cac991d32ff65410728
hobroker <igor.leahu24@gmail.com>
2020-08-28 16:00:16 +0300

    Add IconsConstants

--- lib/constants/asset_constants.dart
+++ lib/constants/asset_constants.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
 @immutable
-class _IconsPath {
-  final spotifyBlack = 'assets/icons/spotify-black.png';
-  final spotifyWhite = 'assets/icons/spotify-white.png';
-}
-
-@immutable
 class AssetConstants {
-  static final icons = _IconsPath();
   static final i18nDir = 'assets/i18n';
 }
--- lib/views/icons/spotify_icon.dart
+++ lib/views/icons/spotify_icon.dart
@@ -1,7 +1,7 @@
-import 'package:thynui/constants/asset_constants.dart';
+import 'package:thynui/constants/icon_constants.dart';
 import 'package:thynui/views/icons/square_icon.dart';
 
 class SpotifyIcon extends SquareIcon {
   @override
-  String get iconPath => AssetConstants.icons.spotifyWhite;
+  String get iconPath => IconsConstants.spotifyWhite;
 }
